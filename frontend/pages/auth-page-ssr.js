16946b7c3b1a75fa2f0464394a02d4db0f571b05
Eliel Sousa <sousaeliel@gmail.com>
2022-11-25 08:55:28 -0300

    create project

---
+++ frontend/pages/auth-page-ssr.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import nookies from 'nookies';
+import { tokenService } from '../src/services/auth/tokenService';
+
+function AuthPageSSR(props) {
+
+  return (
+    <div>
+      <h1>
+        Auth Page Server Side Render
+      </h1>
+      <pre>
+        {JSON.stringify(props, null, 2)}
+      </pre> 
+    </div>
+  )
+}
+
+export default AuthPageSSR;
+
+export async function getServerSideProps(ctx) {
+  const cookies = nookies.get(ctx)
+  console.log('cookies', cookies);
+
+  return {
+    props: {
+      token: tokenService.get(ctx),
+    },
+  }
+}
+
